c3764b9059351a86dfda7834fbb10b06e7cc37d0
jteresco <terescoj@gmail.com>
2022-11-03 11:03:01 -0400

    Fall 2022 updates.

--- exec/exec2nonull.c
+++ exec/exec2nonull.c
@@ -9,6 +9,7 @@
 
   Updated February 2010, Mount Holyoke College, CS 322
   Updated October 2020, Siena College, CSIS 330
+  Updated November 2022, Siena College, CSIS 330
 */
 
 #include <stdio.h>
@@ -26,10 +27,17 @@ int main() {
   perror("execvp");
 
   /* that probably failed -- let's see how it goes if we don't go too
-     far off the end of our array */
+     far off the end of our array, but still go off the end */
   argv[4] = NULL;
   execvp("./procinfo", argv);
-  printf("So this line never executes unless there's an error:\n");
+  printf("So this line also never executes unless there's an error:\n");
+  perror("execvp");
+
+  /* that probably failed -- let's see how it goes if we drop in
+     the NULL just off the end of our array */
+  argv[3] = NULL;
+  execvp("./procinfo", argv);
+  printf("So this line also also never executes unless there's an error:\n");
   perror("execvp");
   return 1;
 }
